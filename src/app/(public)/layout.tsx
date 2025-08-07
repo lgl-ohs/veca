144a8a4cd1fb518b78b881abc8c95052c6f690cc
lgliao <lgliao@ls-mbp-2>
2025-08-07 15:13:59 +0100

    Removed the use of auth session in public layout.

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,6 +3,8 @@ import { ReactNode } from 'react';
 export default function PublicLayout(props: {children: ReactNode}) {
 
   return (
-    {props.children}
+    <main>
+      {props.children}
+    </main>
   )
 }
